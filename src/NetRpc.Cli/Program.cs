0f4118c20fd218295daa9c47543e716dd48235d2
Ade Attwood <ade@gl2tech.com>
2026-02-10 11:26:54 +0000

    Add lint command to the cli

--- src/NetRpc.Cli/Program.cs
+++ src/NetRpc.Cli/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 
 using System.Globalization;
 
@@ -9,4 +9,14 @@ using Spectre.Console.Cli;
 CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
 
 var app = new CommandApp<CodeGenCommand>();
+
+app.Configure(config =>
+{
+    config.AddCommand<CodeGenCommand>("generate")
+        .WithDescription("Generate code from thrift definitions");
+
+    config.AddCommand<LintCommand>("lint")
+        .WithDescription("Lint thrift definitions");
+});
+
 return app.Run(args);
